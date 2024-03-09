17005383ca9af4335282adff232cff837fe0076d
Muhinde234 <igirimpuhwedosta@gmail.com>
2024-03-09 11:05:53 +0200

    code structure

---
+++ index.js
@@ -0,0 +1,6 @@
+//code structure 
+//this one is hard to read
+console.log(" I am learning javascript"); console.log("progressing");
+// this one is better 
+console.log("I am learning javascript");
+console.log("progressing");
